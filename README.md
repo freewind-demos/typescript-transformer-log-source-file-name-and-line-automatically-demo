d5d5c800003fbbed6f1db82b0c8d2dde8758ff09
Freewind <nowindlee@gmail.com>
2020-12-10 12:34:56 +0800

    typescript-transformer-log-function-entry-automatically-demo

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-TypeScript Hello World Demo
-===========================
+TypeScript Transformer Log Function Entry Automatically Demo
+=============================================================
+
+能否利用typescript的transformer在AST层面做一些事情，让我们以一种简单的方式来自动log一个函数的入口和参数
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,17 @@
-console.log('Hello');
+function log(...params: any[]): void {
+  console.log(...params)
+}
+
+function hello(name: string) {
+  // have to log the entry manually
+  // Is it possible to log it automatically? Or just doing some general setup?
+  // It can get:
+  //  1. the function name
+  //  2. the params names and values
+  // Just like generating the following line manually
+  log('> hello', { name });
+  
+  console.log(`Hello, ${name}`);
+}
+
+hello('typescript');
